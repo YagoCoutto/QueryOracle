97add219c87599a8e5968971d2c07c3c9543909b
Yago <62577739+YagoCoutto@users.noreply.github.com>
2023-09-26 17:59:37 -0300

    update

--- R121PED.sql
+++ R121PED.sql
@@ -25,18 +25,20 @@ SELECT
     e120ped.datblo       AS data,
     trunc(e120ped.horblo / 60) || ':'|| mod(e120ped.horblo, 60) AS hora,   
     CASE
-        WHEN e120ped.sitped = 1
-             AND e120ped.pedblo = 'N' THEN 'Imprimir'
+        WHEN e120ped.sitped = 1 || 4  AND e120ped.pedblo = 'S' THEN 'Imprimir'
         WHEN to_char(usu_tctrped.usu_datimp, 'DD/MM/YYYY') = '31/12/1900' THEN 'Pedido bloq.'
         ELSE
             to_char(usu_tctrped.usu_datimp, 'DD/MM/YYYY')
     END AS dat_imp,   
+  
     trunc(usu_tctrped.usu_horimp / 60) || ':' || mod(usu_tctrped.usu_horimp, 60) AS hora_imprimido,
     e120ped.usu_libfat   AS liberado_fat,
     e140ipv.numnfv       AS nota_fiscal,
     e140ipv.codsnf       AS serie,
-    e140ipv.datger       AS data,
-    e140ipv.horger       AS hora,
+    CASE 
+        WHEN E120PED.USU_LIBFAT = 'S' AND e120ped.sitped = 1 THEN 'Faturar'
+        ELSE to_char(e140ipv.datger, 'DD/MM/YYYY') END AS Data,
+    trunc (e140ipv.horger / 60) || ':' || mod(e140ipv.horger,60)       AS hora,
     e120ped.usuger       AS cod_usu,
     r999usu.nomusu       AS usuario,
     e073tra.nomtra       AS transportadora
@@ -48,9 +50,11 @@ FROM
                                  AND sapiens.e120ped.codfil = sapiens.usu_tctrped.usu_codfil
                                  AND sapiens.e120ped.numped = sapiens.usu_tctrped.usu_numped
                                  AND sapiens.e120ped.datemi = sapiens.usu_tctrped.usu_datimp
+                                 AND sapiens.usu_tctrped.usu_seqins = sapiens.usu_tctrped.usu_seqins
     LEFT JOIN sapiens.e140ipv ON sapiens.e120ped.codemp = sapiens.e140ipv.codemp
                                  AND sapiens.e120ped.codfil = sapiens.e140ipv.codfil
                                  AND sapiens.e140ipv.numped = sapiens.e120ped.numped
+
     LEFT JOIN sapiens.r999usu ON sapiens.e120ped.usuger = sapiens.r999usu.codusu
     LEFT JOIN sapiens.e073tra ON sapiens.e120ped.codtra = sapiens.e073tra.codtra
     LEFT JOIN sapiens.e085ent ON sapiens.e120ped.codcli = sapiens.e085ent.codcli
@@ -58,8 +62,8 @@ FROM
 WHERE
     e120ped.codemp = 25
     AND e120ped.codfil = 1
-    AND e120ped.datblo = to_date('22/09/2023','DD/MM/YYYY')
-    AND 1 = 1
+    AND e120ped.datblo >= to_date('26/09/2023','DD/MM/YYYY') and e120ped.datblo <= to_date('26/09/2023','DD/MM/YYYY')
+    
 GROUP BY
     e120ped.sitped,
     e120ped.numped,
@@ -91,8 +95,12 @@ GROUP BY
     e120ped.horblo,
     usu_tctrped.usu_datimp,
     usu_tctrped.usu_horimp,
-    e120ped.vlrliq
+    e120ped.vlrliq,
+    sapiens.usu_tctrped.usu_seqins
 ORDER BY
     e120ped.sitped,
     e120ped.numped,
-    e085cli.apecli
+    e085cli.apecli,
+    e120ped.codemp,
+    usu_tctrped.usu_seqins,
+    sapiens.e140ipv.numnfv
