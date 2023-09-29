6ddcabea26112f51c1f11021d6b29fea81e88b99
Yago <62577739+YagoCoutto@users.noreply.github.com>
2023-09-29 16:00:08 -0300

    new update

--- R121PED.sql
+++ R121PED.sql
@@ -19,24 +19,24 @@ SELECT
     END AS vlrliq,
     e120ped.sitped       AS sit_pedido,
     e120ped.horger       AS h_dig,
-    trunc(e120ped.horfec / 60)
+    TRUNC(e120ped.horfec / 60)
     || ':'
-    || mod(e120ped.horfec, 60) AS hora_fech,
+    || MOD(e120ped.horfec, 60) AS hora_fech,
     e120ped.datprv       AS previsao,
     e120ped.pedblo       AS bloq,
-    e120ped.datblo       AS data,
-    trunc(e120ped.horblo / 60)
+    e120ped.datblo       AS DATA,
+    TRUNC(e120ped.horblo / 60)
     || ':'
-    || mod(e120ped.horblo, 60) AS hora,
+    || MOD(e120ped.horblo, 60) AS hora,
     CASE
-        WHEN to_char(usu_tctrped.usu_datimp, 'DD/MM/YYYY') = '31/12/1900' THEN
-            ' '
+        WHEN to_char(usu_tctrped.usu_datimp, 'DD/MM/YYYY') = '31/12/1900' AND E120PED.SITPED =1 AND E120PED.PEDBLO = 'N' THEN
+            'imprimir'
         ELSE
             nvl(to_char(usu_tctrped.usu_datimp, 'DD/MM/YYYY'), ' ')
     END AS dat_imp,
-    trunc(usu_tctrped.usu_horimp / 60)
+    TRUNC(usu_tctrped.usu_horimp / 60)
     || ':'
-    || mod(usu_tctrped.usu_horimp, 60) AS hora_imprimido,
+    || MOD(usu_tctrped.usu_horimp, 60) AS hora_imprimido,
     e120ped.usu_libfat   AS liberado_fat,
     nvl(e140ipv.numnfv, e140isv.numnfv) AS nota_fiscal,
     nvl(e140ipv.codsnf, e140isv.codsnf) AS serie,
@@ -46,28 +46,25 @@ SELECT
             'Faturar'
         ELSE
             nvl(to_char(e140ipv.datger, 'DD/MM/YYYY'), to_char(e140isv.datger, 'DD/MM/YYYY'))
-    END AS data,
-    nvl(trunc(e140ipv.horger / 60)
-        || mod(e140ipv.horger, 60), trunc(e140isv.horger / 60)
+    END AS DATA,
+    nvl(TRUNC(e140ipv.horger / 60)
+        || MOD(e140ipv.horger, 60), TRUNC(e140isv.horger / 60)
                                     || ':'
-                                    || mod(e140isv.horger, 60)) AS hora,
+                                    || MOD(e140isv.horger, 60)) AS hora,
     e120ped.usuger       AS cod_usu,
     r999usu.nomusu       AS usuario,
     e073tra.nomtra       AS transportadora
 FROM
-    sapiens.e120ped left
+    sapiens.e120ped LEFT
     JOIN sapiens.e085cli ON sapiens.e120ped.codcli = sapiens.e085cli.codcli
     LEFT JOIN sapiens.e070emp ON sapiens.e120ped.codemp = sapiens.e070emp.codemp
     LEFT JOIN sapiens.usu_tctrped ON sapiens.e120ped.codemp = sapiens.usu_tctrped.usu_codemp
                                      AND sapiens.e120ped.codfil = sapiens.usu_tctrped.usu_codfil
                                      AND sapiens.e120ped.numped = sapiens.usu_tctrped.usu_numped
                                      AND usu_tctrped.usu_seqins = (
-        SELECT
-            MIN(usu_seqins)
-        FROM
-            usu_tctrped tctrped2
-        WHERE
-            tctrped2.usu_seqins = 1)
+                                     SELECT MIN(usu_seqins) 
+                                     FROM usu_tctrped tctrped2 
+                                     WHERE tctrped2.usu_codemp = usu_tctrped.usu_codemp AND tctrped2.usu_codfil = usu_tctrped.usu_codfil AND tctrped2.usu_numped = usu_tctrped.usu_numped)
     LEFT JOIN sapiens.e140ipv ON sapiens.e120ped.codemp = sapiens.e140ipv.codemp
                                  AND sapiens.e120ped.codfil = sapiens.e140ipv.codfil
                                  AND sapiens.e140ipv.numped = sapiens.e120ped.numped
@@ -75,9 +72,9 @@ FROM
         SELECT
             MIN(seqipv)
         FROM
-            e140ipv e140ipv2
+            sapiens.e140ipv e140ipv2
         WHERE
-            e140ipv.seqipv = 1
+            sapiens.e140ipv2.codemp = e140ipv.codemp AND e140ipv2.codfil = e140ipv.codfil AND e140ipv2.numped = e140ipv.numped
     )
     LEFT JOIN sapiens.e140isv ON sapiens.e140isv.codemp = sapiens.e120ped.codemp
                                  AND sapiens.e140isv.codfil = sapiens.e120ped.codfil
@@ -86,9 +83,9 @@ FROM
         SELECT
             MIN(seqisv)
         FROM
-            e140isv 
+            sapiens.e140isv e140isv2
         WHERE
-            e140isv.seqisv = 1
+            e140isv2.codemp = e140isv.codemp AND e140isv2.codfil = e140isv2.codfil AND e140isv2.numped = e140isv.numped
     )
     LEFT JOIN sapiens.r999usu ON sapiens.e120ped.usuger = sapiens.r999usu.codusu
     LEFT JOIN sapiens.e073tra ON sapiens.e120ped.codtra = sapiens.e073tra.codtra
@@ -97,7 +94,7 @@ FROM
 WHERE
     e120ped.codemp BETWEEN 1 AND 29
     AND e120ped.codfil BETWEEN 1 AND 9
-    AND e120ped.datblo BETWEEN TO_DATE('28/09/2023', 'DD/MM/YYYY') AND TO_DATE('28/09/2023', 'DD/MM/YYYY')
+    AND e120ped.datblo BETWEEN TO_DATE('29/09/2023', 'DD/MM/YYYY') AND TO_DATE('29/09/2023', 'DD/MM/YYYY')
 ORDER BY
     e120ped.sitped,
     e120ped.numped,
